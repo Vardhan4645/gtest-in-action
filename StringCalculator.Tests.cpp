82a618037f9687212675f14754c0f6903262c577
Vardhan4645 <tejavardhan4645@gmail.com>
2024-09-17 15:02:36 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,11 +1,12 @@
 #include "StringCalculator.h"
 #include <gtest/gtest.h>
 
-TEST(StringCalculator,add_empty(InputString_ZeroIsExpected){
-//arrange
-string input="";
-int expectedValue=0;
-//Act
-int actualValue=Add(input);
-//Assert
-ASSERT_EQ(actualValue,expectedValue);
+TEST(StringCalculatorTestSuite,Add_emptyInputString_ZeroIsExpected){
+  //Arrange
+  string input = "";
+  int expectedValue=0;
+  //Act
+  int actualValue=Add(input);
+  //Assert
+  ASSERT_EQ(actualValue,expectedValue);
+}
