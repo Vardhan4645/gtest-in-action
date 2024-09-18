0945cd3af2c06f77bf9c5cdb8cdb7d52af9f191a
Vardhan4645 <tejavardhan4645@gmail.com>
2024-09-18 17:43:44 +0530

    Create TestChallenge.cpp

---
+++ TestChallenge.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+#include "Testchallenge.h"
+ 
+int alertFailureCount = 0;
+
+int getFailureCount(){
+return alertFailureCount;
+}
+
+void alertInCelcius(float farenheit,INetworkAlerter *networkAlerterPtr) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlerterPtr->alert(celcius);
+    if (returnCode != 200) {
+        alertFailureCount += 1;
+    }
+}
+
+int alertInCelciusFacade(float farenheit,INetworkAlerter *networkAlerterPtr){
+   alertInCelcius(farenheit,networkAlerterPtr);
+   return getFailureCount();
+ }
+ 
