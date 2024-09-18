289bf3b10256868f3a2bd0c675d37d4953bc5d59
Vardhan4645 <tejavardhan4645@gmail.com>
2024-09-18 11:57:34 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,7 +2,8 @@
 #include <vector>
 #include <numeric>
 #include <regex>
+#include <iostream>
 
 using namespace std;
 //int Add(const char* input);
-int Add(const string &input);
+void Add(const string &input);
